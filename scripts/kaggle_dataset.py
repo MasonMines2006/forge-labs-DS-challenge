0173971977165703ff76e3c7b028ef1aa3340b0a
MasonMines2006 <mmines@unc.edu>
2025-01-30 10:38:24 -0500

    Successfully loaded data-set, working on reading and analysis

--- scripts/kaggle_dataset.py
+++ scripts/kaggle_dataset.py
@@ -1,9 +1,6 @@
 import kagglehub
 
-# Specify the target folder where you want to save the dataset
-target_folder = "/Users/mmines/PycharmProjects/forge-labs-DS-challenge/data"
-
-# Download the latest version of the dataset and save it to the target folder
-path = kagglehub.dataset_download("paulchambaz/google-street-view", target_folder)
+# Download latest version
+path = kagglehub.dataset_download("paulchambaz/google-street-view")
 
 print("Path to dataset files:", path)
